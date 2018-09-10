d76b94491f3b7b10f1f9466a960af46254fad9d5
Juan Camilo Rojas <juankdrums14@gmail.com>
2018-09-10 15:29:34 -0500

    Entrenamiento del 8 de septiembre

--- 8sept/1033.cpp
+++ 8sept/1033.cpp
@@ -2,44 +2,57 @@
 
 using namespace std;
 
-void multmat(long long int m1[3][3], long long int m2[3][3], long long int r[3][3], long long int b)
+vector<vector<unsigned long long> > multmat(vector<vector<unsigned long long> > m1, vector<vector<unsigned long long> > m2, unsigned long long b)
 {
+  vector<vector<unsigned long long> > r(3,vector<unsigned long long>(3,0));
   for(int i = 0; i < 3; i++)
   {
     for(int j = 0; j < 3; j++)
     {
-      r[i][j] = 0;
       for(int k = 0; k < 3; k++)
       {
-        r[i][j] = (r[i][j] + ((m1[i][k]%b)*(m2[k][j]%b)%b))%b;
+        r[i][j] = (r[i][j] + ((m1[i][k]%b)*(m2[k][j]%b))%b)%b;
       }
     }
   }
+  return r;
 }
 
 
-void pow(long long int m1[3][3], long long int n, long long int r[3][3], long long int b)
+vector<vector<unsigned long long> > pot(vector<vector<unsigned long long> > m1, unsigned long long n, unsigned long long b)
 {
+  vector <vector<unsigned long long> > r(3,vector<unsigned long long>(3,1));
+  while(n > 0)
+  {
+    if(n % 2 != 0)
+    {
+      r = multmat(r,m1,b);
+    }
+    m1 = multmat(m1,m1,b);
+    n = n/2;
+  }
+  return r;
+  /*if(n == 0)
+  {
+    vector <vector<unsigned long long> > r(3,vector<unsigned long long>(3,1));
+    return r;
+  }
   if(n == 1)
   {
-    for(int i = 0; i < 3; i++)
-    {
-      for(int j = 0; j < 3; j++)
-      {
-        r[i][j] = m1[i][j];
-      }
-    }
-    return;
+    return m1;
   }
-  long long int aux[3][3];
+  vector<vector<unsigned long long> > aux;
+  vector<vector<unsigned long long> > aux2;
+  aux = pot(m1,n/2,b);
   if(n % 2 == 0)
   {
-    pow(m1,n/2,aux,b);
-    multmat(aux,aux,r,b);
-    return;
+    aux2 = multmat(aux,aux,b);
+    return multmat(aux2,m1,b);
   }
-    pow(m1,n-1,aux,b);
-    multmat(aux,m1,r,b);
+  else
+  {
+      return multmat(aux,aux,b);
+  }*/
 }
 
 
@@ -51,27 +64,24 @@ int main(int argc, char const *argv[]) {
     {
       break;
     }
-    long long int r[3][3];
-    long long int m[3][3];
-    m[0][0] = 1;
-    m[0][1] = 1;
-    m[0][2] = 1;
-    m[1][0] = 1;
+    vector<vector<unsigned long long> > m(3,vector<unsigned long long>(3,1));
     m[1][1] = 0;
     m[1][2] = 0;
     m[2][0] = 0;
     m[2][1] = 0;
-    m[2][2] = 1;
+    //cout << "holi" << endl;
     //cout << r[0][0] << endl;
-    long long int res;
+    unsigned long long res = 0;
     if(n == 0 || n == 1)
     {
       res = 1;
     }
     else
     {
-      pow(m,n-1,r,b);
-      res = r[0][0]%b + r[0][1]%b + r[0][2]%b;
+      vector<vector<unsigned long long> > r;
+      r = pot(m,n-2,b);
+      res = (r[0][0]%b + r[0][1]%b + r[0][2]%b)%b;
+      //res = r[0][0];
     }
     cout << "Case " << cont << ": " << n << " " << b << " " << res << endl;
     cont++;
