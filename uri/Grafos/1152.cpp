2b8181faf7db5c74d9550515bd15d92733f8aa38
Juan Camilo Rojas <juankdrums14@gmail.com>
2018-10-02 00:25:42 -0500

    Kruskal con Disjoint set union.

--- uri/Grafos/1152.cpp
+++ uri/Grafos/1152.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
-
-
 using namespace std;
-
+vector<int> parent,sizee;
 struct Edge{
   int u,v,weight;
   bool operator < (Edge const& other)
@@ -11,32 +9,52 @@ struct Edge{
   }
 };
 
+void make_set(int v)
+{
+  parent[v] = v;
+  sizee[v] = 1;
+}
+
+int find_set(int v)
+{
+  if(v == parent[v]) return v;
+  return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b)
+{
+  a = find_set(a);
+  b = find_set(b);
+  if(a != b)
+  {
+    if(sizee[a] < sizee[b]) swap(a,b);
+    parent[b] = a;
+    sizee[a] += sizee[b];
+  }
+}
+
 int main(int argc, char const *argv[]) {
   int n=1,m=1;
   while(cin >> m >> n && n && m)
   {
     vector<Edge> v(n);
     vector<Edge> result;
+    parent.resize(m);
+    sizee.resize(m);
     int costin = 0;
     for(int i = 0; i < n; i++) {cin >> v[i].u >> v[i].v >> v[i].weight; costin += v[i].weight;}
     sort(v.begin(),v.end());
-    int tree_id[m];
-    for(int i = 0; i < m; i++) tree_id[i] = i;
+    for(int i = 0; i < m; i++) make_set(i);
     int cost = 0;
     for(int i = 0; i < n; i++)
     {
-      if(tree_id[v[i].u] != tree_id[v[i].v])
+      if(find_set(v[i].u) != find_set(v[i].v))
       {
         result.push_back(v[i]);
         cost += v[i].weight;
-        int old_id = tree_id[v[i].u], new_id = tree_id[v[i].v];
-        for(int j = 0; j < m; j++)
-        {
-          if(tree_id[j] == old_id) tree_id[j] = new_id;
-        }
+        union_sets(v[i].u,v[i].v);
       }
     }
-
     int aho = costin - cost;
     cout << aho << endl;
 
